6ce238e5e304c0a43d545c3ba337ca50676e92c4
Razortx1 <85193546+Razortx1@users.noreply.github.com>
2022-05-25 16:53:06 -0400

    Initial Commit

---
+++ src/main/java/com/aristos_propiedades/aristos_propiedades/AristosPropiedadesApplication.java
@@ -0,0 +1,13 @@
+package com.aristos_propiedades.aristos_propiedades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AristosPropiedadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AristosPropiedadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aristos_propiedades/aristos_propiedades/AristosPropiedadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aristos_propiedades.aristos_propiedades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AristosPropiedadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
