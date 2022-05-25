ad6f7dddce315f00715151200fd1d23f4c8ae875
Razortx1 <85193546+Razortx1@users.noreply.github.com>
2022-05-25 18:10:21 -0400

    Creacion de modelos

--- src/main/java/com/aristos_propiedades/aristos_propiedades/model/Noticias.java
+++ src/main/java/com/aristos_propiedades/aristos_propiedades/model/Noticias.java
@@ -1,5 +1,58 @@
 package com.aristos_propiedades.aristos_propiedades.model;
 
+import java.sql.Blob;
+import java.sql.Date;
+
 public class Noticias {
+    private Integer id_noticia;
+    private String mensaje;
+    private Blob imagen_noticia;
+    private Date fecha_noticia;
+    private Integer id_tipo_noticias;
+    private Integer id_user;
+    public Noticias() {
+    }
+    public Noticias(String mensaje, Blob imagen_noticia, Date fecha_noticia) {
+        this.mensaje = mensaje;
+        this.imagen_noticia = imagen_noticia;
+        this.fecha_noticia = fecha_noticia;
+    }
+    public Integer getId_noticia() {
+        return id_noticia;
+    }
+    public void setId_noticia(Integer id_noticia) {
+        this.id_noticia = id_noticia;
+    }
+    public String getMensaje() {
+        return mensaje;
+    }
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    public Blob getImagen_noticia() {
+        return imagen_noticia;
+    }
+    public void setImagen_noticia(Blob imagen_noticia) {
+        this.imagen_noticia = imagen_noticia;
+    }
+    public Date getFecha_noticia() {
+        return fecha_noticia;
+    }
+    public void setFecha_noticia(Date fecha_noticia) {
+        this.fecha_noticia = fecha_noticia;
+    }
+    public Integer getId_tipo_noticias() {
+        return id_tipo_noticias;
+    }
+    public void setId_tipo_noticias(Integer id_tipo_noticias) {
+        this.id_tipo_noticias = id_tipo_noticias;
+    }
+    public Integer getId_user() {
+        return id_user;
+    }
+    public void setId_user(Integer id_user) {
+        this.id_user = id_user;
+    }
     
+
 }
--- src/main/java/com/aristos_propiedades/aristos_propiedades/model/Usuario.java
+++ src/main/java/com/aristos_propiedades/aristos_propiedades/model/Usuario.java
@@ -1,5 +1,40 @@
 package com.aristos_propiedades.aristos_propiedades.model;
 
 public class Usuario {
+    private Integer id_user;
+    private String Nombre;
+    private String Contraseña;
+    private String id_rol;
+    public Usuario() {
+    }
+    public Usuario(String nombre, String contraseña) {
+        Nombre = nombre;
+        Contraseña = contraseña;
+    }
+    public Integer getId() {
+        return id_user;
+    }
+    public void setId(Integer id) {
+        this.id_user = id;
+    }
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public String getContraseña() {
+        return Contraseña;
+    }
+    public void setContraseña(String contraseña) {
+        Contraseña = contraseña;
+    }
+    public String getId_rol() {
+        return id_rol;
+    }
+    public void setId_rol(String id_rol) {
+        this.id_rol = id_rol;
+    }
+
     
 }
