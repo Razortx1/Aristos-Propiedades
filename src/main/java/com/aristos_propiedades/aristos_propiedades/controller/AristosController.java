7c6f32adef7afcad25b4d4cd64bd27e030eaa821
Razortx1 <85193546+Razortx1@users.noreply.github.com>
2022-05-30 22:14:06 -0400

    Cambios en el index.html

--- src/main/java/com/aristos_propiedades/aristos_propiedades/controller/AristosController.java
+++ src/main/java/com/aristos_propiedades/aristos_propiedades/controller/AristosController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AristosController {
     @GetMapping("/index")
     public String index() {
-        return "html/login";
+        return "html/index";
     }
     
 }
